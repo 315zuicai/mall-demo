1a03e81f9e7e3ffa1e0e43c25b470c812a8ade2b
丁开强 <dingkaiqiang@dkq.local>
2020-09-30 22:06:06 +0800

    update: 更新

--- src/network/request.js
+++ src/network/request.js
@@ -4,7 +4,7 @@ export function request(config) {
   
     // 1.创建axios的实例
   const instance = axios.create({
-    baseURL: "此处不外露" ,
+    // baseURL: "此处不外露" ,
     // baseURL: "此处不外露" ,
     timeout: 5000
   });
